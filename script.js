6638d7568c3cd0d3fe8e5e5d328bb194bcb1ce89
jdl088 <jdl088@users.noreply.github.com>
2025-06-14 18:43:39 +0200

    Update script.js
    funny button

--- script.js
+++ script.js
@@ -1,9 +1,41 @@
 const stadtcodeEl = document.getElementById("stadtcode");
 const resultatEl = document.getElementById("resultat");
-const kennzeichenImg = document.getElementById("kennzeichen-img");
+const funnyBtn = document.getElementById("funny-btn");
 
-const stadtcodes = ["A", "AA", "AB", "AC", "AE", "AH", "AIB", "AK", "AM", "AN", "ANA", "ANG", "ANK", "A\u00d6", "AP", "ARN", "ART", "AS", "ASL", "ASZ", "AT", "AU", "AUR", "AW", "AZ", "AZE", "B", "BA", "BAD", "BAR", "BB", "BBG", "BC", "BGL", "BI", "BIR", "BIT", "BK", "BL", "BLB", "BM", "BN", "BO", "BOR", "BOT", "BRA", "BRB", "BS", "BT", "B\u00dcD", "BUL", "BUZ", "BZ", "C", "CA", "CB", "CE", "CLP", "CO", "COC", "CUX", "CW", "D", "DA", "DAH", "DAN", "DAU", "DBR", "DD", "DE", "DEL", "DGF", "DH", "DL", "DLG", "DM", "DN", "DO", "DON", "DU", "D\u00dcW", "DW", "DZ", "E", "EA", "EBE", "EF", "EG", "EI", "EIC", "EIL", "EL", "EM", "EMS", "EN", "ER", "ERB", "ES", "ESW", "EU", "F", "FB", "FD", "FDS", "FF", "FFB", "FG", "FL", "FN", "FO", "FR", "FRG", "FS", "FT", "F\u00dc", "FW", "G", "GA", "GAP", "GE", "GEL", "GEO", "GER", "GF", "GG", "GGT", "GHA", "GI", "GL", "GM", "GN", "G\u00d6", "GP", "GR", "GS", "GT", "GTH", "G\u00dc", "GZ", "H", "HA", "HAL", "HAM", "HAS", "HB", "HBN", "HD", "HDH", "HE", "HEB", "HEF", "HEI", "HEL", "HER", "HF", "HG", "HH", "HI", "HL", "HM", "HN", "HO", "HOG", "HOL", "HOM", "HOR", "HP", "HR", "HS", "HSK", "HU", "HVL", "HWI", "HX", "HY", "IGB", "IK", "IN", "IZ", "J", "JE", "JL", "K", "KA", "KB", "KC", "KE", "KIB", "KI", "KLE", "KL", "K\u00d6N", "KR", "KS", "KT", "K\u00dcN", "KUS", "KW", "KYF", "L", "LA", "LAN", "LB", "LD", "LDK", "LDS", "LER", "LEV", "LG", "LI", "LIF", "LIP", "LL", "LM", "LN", "LOS", "LP", "LR", "LSA", "LSN", "LU", "L\u00dcN", "LWL", "M", "MA", "MAIN", "MB", "MD", "ME", "MEI", "MEK", "MER", "MG", "MH", "MI", "MIL", "MK", "ML", "MN", "MO", "MOD", "MOS", "MSE", "MSP", "MST", "MTK", "MTL", "M\u00dc", "MYK", "MZ", "N", "NAI", "ND", "NE", "NEA", "NEW", "NF", "NH", "NI", "NL", "NM", "NMS", "N\u00d6", "NOH", "NOM", "NOR", "NP", "NR", "NU", "NVP", "NW", "NWM", "NY", "OA", "OB", "OBG", "OD", "OE", "OF", "OG", "OH", "OHV", "OL", "OPR", "OS", "OSL", "OVP", "P", "PA", "PAN", "PB", "PCH", "PE", "PF", "PI", "PIR", "PL", "PL\u00d6", "PM", "PR", "PS", "QLB", "R", "RA", "RC", "RD", "RE", "REG", "RH", "RO", "ROW", "RS", "RT", "RU", "RV", "RW", "RZ", "S", "SA", "SAD", "SAL", "SANK", "SB", "SC", "SDL", "SDT", "SE", "SEE", "SG", "SHA", "SHG", "SHK", "SHL", "SI", "SIG", "SIM", "SK", "SL", "SLE", "SLF", "SM", "SN", "SO", "S\u00d6M", "SOK", "SP", "SPN", "SR", "ST", "STA", "STD", "SU", "S\u00dcW", "SW", "SZ", "TBB", "TDO", "TF", "THL", "TIR", "TO", "T\u00d6L", "TR", "TS", "T\u00dc", "UE", "UEM", "UL", "UM", "UN", "USI", "V", "VB", "VEC", "VER", "VG", "VIE", "VK", "VS", "W", "WAF", "WAK", "WB", "WE", "WEN", "WER", "WF", "WHV", "WI", "WIL", "WIS", "WL", "WM", "WN", "WOB", "WOH", "WOL", "WOR", "WR", "WSF", "WST", "WT", "WTM", "W\u00dc", "WUN", "WUR", "WW", "Z", "ZE", "ZI", "ZW"];
+const stadtcodes = ["B", "M", "HH", "S", "K", "F", "AA", "AC", "BIT", "DON", "ES", "ER", "GÖ", "UL"];
 
+// Lustige Kennzeichen-Kombinationen mit Wortspielen
+const funnyKennzeichen = [
+  // Deutsche Wortspiele
+  { code: "B", letters: "AD", numbers: "42", joke: "B-AD 42 - Bad luck!" },
+  { code: "M", letters: "EH", numbers: "123", joke: "M-EH 123 - Meh... 🤷‍♂️" },
+  { code: "HH", letters: "MM", numbers: "69", joke: "HH-MM 69 - Hmmm... 🤔" },
+  { code: "S", letters: "OS", numbers: "911", joke: "S-OS 911 - SOS! Hilfe!" },
+  { code: "K", letters: "O", numbers: "1337", joke: "K-O 1337 - Knockout Elite!" },
+  { code: "F", letters: "UN", numbers: "4", joke: "F-UN 4 - Fun for all!" },
+  { code: "AA", letters: "H", numbers: "1", joke: "AA-H 1 - Ahhh, nummer eins!" },
+  { code: "AC", letters: "DC", numbers: "220", joke: "AC-DC 220 - Highway to Hell!" },
+  { code: "BIT", letters: "E", numbers: "8", joke: "BIT-E 8 - Byte Eight!" },
+  { code: "DON", letters: "T", numbers: "404", joke: "DON-T 404 - Not Found!" },
+  { code: "ES", letters: "S", numbers: "3", joke: "ES-S 3 - Ess drei mal!" },
+  { code: "ER", letters: "R", numbers: "0", joke: "ER-R 0 - Error Zero!" },
+  { code: "GÖ", letters: "D", numbers: "7", joke: "GÖ-D 7 - Göd Seven!" },
+  { code: "UL", letters: "OL", numbers: "2", joke: "UL-OL 2 - Ultra LOL!" },
+  
+  // Weitere lustige Kombinationen
+  { code: "B", letters: "EE", numbers: "1", joke: "B-EE 1 - Bee Number One! 🐝" },
+  { code: "M", letters: "OO", numbers: "0", joke: "M-OO 0 - Moo! Kuh-l! 🐄" },
+  { code: "HH", letters: "I", numbers: "5", joke: "HH-I 5 - Hi Five! ✋" },
+  { code: "S", letters: "EE", numbers: "4", joke: "S-EE 4 - See you later!" },
+  { code: "K", letters: "OO", numbers: "11", joke: "K-OO 11 - Cool Elf!" },
+  { code: "F", letters: "LY", numbers: "747", joke: "F-LY 747 - Fly like Boeing!" },
+  { code: "AA", letters: "A", numbers: "123", joke: "AA-A 123 - Triple A Rating!" },
+  { code: "AC", letters: "E", numbers: "1", joke: "AC-E 1 - Ace of Spades!" },
+  { code: "BIT", letters: "CH", numbers: "3", joke: "BIT-CH 3 - Bit*h Please!" },
+  { code: "DON", letters: "E", numbers: "99", joke: "DON-E 99 - Done and Done!" }
+];
+
+// Stadtcodes zum Select hinzufügen
 stadtcodes.forEach(code => {
   const opt = document.createElement("option");
   opt.value = code;
@@ -11,15 +43,7 @@ stadtcodes.forEach(code => {
   stadtcodeEl.appendChild(opt);
 });
 
-function generiereKennzeichen(code, buchstaben, zahlen) {
-  return `${code} ${buchstaben} ${zahlen}`;
-}
-
-function updateKennzeichen(kennzeichen) {
-  resultatEl.textContent = kennzeichen;
-  kennzeichenImg.src = `https://dummyimage.com/500x100/ffffff/000000&text=${encodeURIComponent(kennzeichen)}`;
-}
-
+// Normaler Kennzeichen-Generator
 document.getElementById("config-form").addEventListener("submit", (e) => {
   e.preventDefault();
   const stadt = stadtcodeEl.value;
@@ -27,19 +51,28 @@ document.getElementById("config-form").addEventListener("submit", (e) => {
   const zahlen = document.getElementById("zahlen").value;
 
   if (!/^[A-Z]{1,2}$/.test(buchstaben) || !/^\d{1,4}$/.test(zahlen)) {
+    resultatEl.className = "";
     resultatEl.textContent = "Ungültiges Format!";
     return;
   }
 
-  const kennzeichen = generiereKennzeichen(stadt, buchstaben, zahlen);
-  updateKennzeichen(kennzeichen);
+  resultatEl.className = "";
+  resultatEl.textContent = `${stadt} ${buchstaben} ${zahlen}`;
 });
 
-document.getElementById("zufall-button").addEventListener("click", () => {
-  const code = stadtcodes[Math.floor(Math.random() * stadtcodes.length)];
-  const buchstaben = String.fromCharCode(65 + Math.floor(Math.random() * 26)) +
-                     (Math.random() > 0.5 ? String.fromCharCode(65 + Math.floor(Math.random() * 26)) : "");
-  const zahlen = Math.floor(1 + Math.random() * 9999).toString();
-  const kennzeichen = generiereKennzeichen(code, buchstaben, zahlen);
-  updateKennzeichen(kennzeichen);
+// Funny Kennzeichen Generator
+funnyBtn.addEventListener("click", () => {
+  const randomFunny = funnyKennzeichen[Math.floor(Math.random() * funnyKennzeichen.length)];
+  
+  resultatEl.className = "funny-result";
+  resultatEl.innerHTML = `
+    <div>
+      <div style="font-size: 1.4rem; margin-bottom: 0.5rem;">
+        ${randomFunny.code} ${randomFunny.letters} ${randomFunny.numbers}
+      </div>
+      <div style="font-size: 1rem; font-style: italic;">
+        ${randomFunny.joke}
+      </div>
+    </div>
+  `;
 });
